5735ef79f615aff80fa0022f6cd473b0ea502d19
Tomer Shahar <tshahar84@gmail.com>
2017-05-23 22:46:10 +0300

    debug login

--- login.php
+++ login.php
@@ -11,20 +11,25 @@
 	$username=$_POST['username'];
 	$password=$_POST['password'];
 	$sql="SELECT * FROM Users WHERE name='$username' and password='$password'";
-if ($result=mysqli_query($link,$sql)) {
-	$count=mysqli_num_rows($result);
-	if($count>1){
-		$code = 200;
-		$rtn = array('success' => true, 'message' => 'login success');
-		session_start();
+	if ($result=mysqli_query($link,$sql)) {
+		$count=mysqli_num_rows($result);
+		if($count>1){
+			$code = 200;
+			$rtn = array('success' => true, 'message' => 'login success');
+			session_start();
+		}
+		else{
+			$code = 404;
+			$rtn = array('success' => false, 'message' => 'login failed');
+		}
+		http_response_code($code);
+		print json_encode($rtn);
+		mysqli_close($link);
+		
 	}
-	else{
-		$code = 404;
-		$rtn = array('success' => false, 'message' => 'login failed');
+	else {
+		http_response_code(500);
+		print json_encode($rtn);
+		mysqli_close($link);	
 	}
-	http_response_code($code);
-    print json_encode($rtn);
-	mysqli_close($link);
-	
-}
 ?>
